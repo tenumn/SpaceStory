731871bbe9319ed213f166a5b6299926c2fbcb3c
31279 <3127906369@qq.com>
2026-07-29 09:52:15 +0800

    资源

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,21 +7,4 @@ extends Node3D
 
 @onready var player: CharacterBody3D = $Player
 @onready var camera: Camera3D = $Camera3D
-
-
-func _ready() -> void:
-	camera.current = true
-
-
-func _process(delta: float) -> void:
-	if not is_instance_valid(player):
-		return
-
-	# 计算目标相机位置: 在角色上方偏后方
-	var target_pos := player.global_position + Vector3(0, camera_height, camera_distance)
-
-	# 平滑跟随
-	camera.global_position = camera.global_position.lerp(target_pos, camera_smoothing * delta)
-
-	# 始终看向角色
-	camera.look_at(player.global_position, Vector3.UP)
+@onready var grid_map: GridMap = $GridMap
